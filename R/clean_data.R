e8687e314734a88192e9765e782756857ae4f4fe
Catherine Eisenhauer <ceisenhauer@users.noreply.github.com>
2023-12-07 15:02:41 +0100

    planning notes

---
+++ R/clean_data.R
@@ -0,0 +1,6 @@
+# this function should:
+# - clean names (ganitor)
+# - clean strings
+# - clean numerics - check for sep as ,
+# - clean dates - check for xecl style
+# - optionally give list of numeric cols and date cols -- otherwise autosense ("force_numeric" ?)
