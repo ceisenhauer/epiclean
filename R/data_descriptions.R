f5e698b1d8ad46ca08f86aa57cb9a7d7918ea758
Catherine Eisenhauer <ceisenhauer@users.noreply.github.com>
2023-12-12 18:02:53 +0000

    drc practice data

---
+++ R/data_descriptions.R
@@ -0,0 +1,18 @@
+# THE WORLD ----------------------------------------------------------------------------------------
+
+#' The World
+#'
+#' Countries of the World
+#'
+#' @docType data
+#'
+#' @format A `dataframe` with 147,851 observations of 4 fields:
+#' \describe{
+#'  \item{prov}{Province Name}
+#'  \item{zs}{Health Zone Name}
+#'  \item{numsem}{ISO Week Number}
+#'  \item{MALADIE}{Maladie Name}
+#' }
+#'
+#' @keywords dataset
+'drc'
